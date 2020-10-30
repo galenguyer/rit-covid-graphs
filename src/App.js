aa376cca04b3eae934e83dd946a274de0674532c
Galen Guyer <galen@galenguyer.com>
2020-10-30 12:54:17 -0400

    Add graphs for data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { readString } from "react-papaparse";
 import useSWR from "swr";
 import logo from "./logo.svg";
 import "./App.css";
+import LineGraph from "./LineGraph";
+import BarGraph from "./BarGraph";
 
 const App = () => {
     const url = "https://raw.githubusercontent.com/galenguyer/rit-covid-cases/main/rit-covid-cases.csv";
     const fetcher = (...args) =>
         fetch(...args)
             .then((res) => res.text())
-            .then((res) => readString(res.trim()));
+            .then((res) => readString(res.trim()).data);
 
     const { data: data, error: error } = useSWR(url, fetcher);
 
@@ -21,17 +23,30 @@ const App = () => {
         );
     }
 
+    console.log("data", data);
+
+    const headings = data.shift();
+
+    const totalCases = data.map((element) => {
+      return {
+          date: element[0],
+          count: parseInt(element[3], 10),
+      };
+  });
+
+  const rollingSevenDay = data.map((element) => {
+    return {
+        date: element[0],
+        count: parseInt(element[2], 10),
+    };
+});
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <h2>Total Cases</h2>
+        <LineGraph data={totalCases} />
+        <h2>Rolling 7 Day New Case Total</h2>
+        <BarGraph data={rollingSevenDay} />
         </div>
     );
 };
---
+++ src/LineGraph.js
@@ -0,0 +1,42 @@
+import React, { PureComponent } from "react";
+import { AreaChart, Area, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Label } from "recharts";
+import "./Graph.css";
+
+const LineGraph = (props) => {
+    return (
+        <div className="Graph">
+            <ResponsiveContainer>
+                <AreaChart width={730} height={250} data={props.data} margin={{ top: 15, right: 30, left: 20, bottom: 5 }}>
+                    <defs>
+                        <linearGradient id="count" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="date" interval={7} tick={<CustomizedAxisTick />} height={90} />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Area type="monotone" dataKey="count" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                    <YAxis dataKey="count" type="number">
+                    </YAxis>
+                    <Tooltip />
+                </AreaChart>
+            </ResponsiveContainer>
+        </div>
+    );
+};
+
+class CustomizedAxisTick extends PureComponent {
+    render() {
+        const { x, y, payload } = this.props;
+
+        return (
+            <g transform={`translate(${x},${y})`}>
+                <text className="Graph-Label" x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-40)">
+                    {payload.value}
+                </text>
+            </g>
+        );
+    }
+}
+
+export default LineGraph;
