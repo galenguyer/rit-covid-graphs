599f5dace3b885348323d04dd30e59a5bddd0a7f
Galen Guyer <galen@galenguyer.com>
2020-11-01 14:24:08 -0500

    Make line graph red because scary

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -14,13 +14,13 @@ const LineGraph = (props) => {
                 >
                     <defs>
                         <linearGradient id="count" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                            <stop offset="5%" stopColor="#FF190D" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#FF190D" stopOpacity={0} />
                         </linearGradient>
                     </defs>
                     <XAxis dataKey="date" interval={7} tick={<CustomizedAxisTick />} height={90} />
                     <CartesianGrid strokeDasharray="3 3" />
-                    <Area type="monotone" dataKey="count" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                    <Area type="monotone" dataKey="count" stroke="#FF190D" fillOpacity={1} fill="url(#colorUv)" />
                     <YAxis dataKey="count" type="number"></YAxis>
                     <Tooltip />
                 </AreaChart>
